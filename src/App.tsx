147b0d2c0a4ee8214b1491a8c1c86d54845fa211
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2024-11-06 10:07:32 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex justify-center items-center p-3 w-full bg-yellow-500">
+      <p className="">Hello</p>
+    </main>
+  );
+}
+
+export default App;
