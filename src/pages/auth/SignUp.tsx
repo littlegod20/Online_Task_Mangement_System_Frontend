769bcca1b314d63eb8d69657713de5712e846dc8
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2024-11-06 15:27:23 +0000

    created a redux store

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,25 +1,9 @@
 import Button from "../../components/common/Button";
 import Input from "../../components/common/Input";
 import { Link } from "react-router-dom";
+import { formInputs } from "../../utils/constants";
 
-const formInputs = [
-  {
-    label: "Name",
-    placeholder: "eg. John Doe",
-  },
-  {
-    label: "Email",
-    placeholder: "eg. johndoe@gmail.com",
-  },
-  {
-    label: "Password",
-    placeholder: "Enter strong password",
-  },
-  {
-    label: "Role",
-    placeholder: "eg. User or Admin",
-  },
-];
+
 
 const SignUp = () => {
   return (
@@ -36,7 +20,7 @@ const SignUp = () => {
             <Input name={item.label} placeholder={item.placeholder} />
           ))}
           <div className="w-2/3 lg:w-1/3 pt-4 text-center">
-            <Button title="Submit" />
+            <Button title="Sign In" />
             <p className="text-sm pt-3 text-slate-500">
               Already signed up? login in{" "}
               <Link
---
+++ src/utils/constants.ts
@@ -0,0 +1,18 @@
+export const formInputs = [
+  {
+    label: "Name",
+    placeholder: "eg. John Doe",
+  },
+  {
+    label: "Email",
+    placeholder: "eg. johndoe@gmail.com",
+  },
+  {
+    label: "Password",
+    placeholder: "Enter strong password",
+  },
+  {
+    label: "Role",
+    placeholder: "eg. User or Admin",
+  },
+];
